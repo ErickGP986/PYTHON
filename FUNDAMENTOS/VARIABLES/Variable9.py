d232b23e53228044db2f8e445ee1ba583fe569d4
ErickGP986 <Pichitron2004@gmail.com>
2025-07-28 12:35:16 -0600

    Create Variable9.py

---
+++ FUNDAMENTOS/VARIABLES/Variable9.py
@@ -0,0 +1,11 @@
+"""""
+Desempaquetado
+Dada la tupla datos = ("Ana", 25, 1.65), desempaquétala en tres variables (nombre, edad, altura) e imprímelas.
+"""""""""
+datos = ("Ana", 25, 1.65) #Cramos la tupla
+
+(nombre, edad, altura) = datos #Le asignamos las variables a las tuplas
+#Lo imprimimos
+print("tu nombre es: ", nombre)
+print("Tu edad es: " ,edad)
+print("Tu altura es: ", altura)
