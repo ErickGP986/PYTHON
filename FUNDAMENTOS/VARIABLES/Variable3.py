6a47b091dd82460e144050c827632e3f220d704a
ErickGP986 <Pichitron2004@gmail.com>
2025-07-28 12:30:27 -0600

    Update Variable3.py

--- FUNDAMENTOS/VARIABLES/Variable3.py
+++ FUNDAMENTOS/VARIABLES/Variable3.py
@@ -1,1 +1,10 @@
+#Declaracion de variables
+Nombre = input("Escriba su Nombre: ")
+Apellido = input("Escriba su apellido: ")
+#Variable que muestre el nombre completo
+nombre_competo = Nombre + " " + Apellido
+#Imprmimos el resultado
+print(f"su nombre es: {Nombre}, su apellido es: {Apellido} y su nombre completo es: {nombre_competo}")
 
+#usa f-string
+print(f"Hola, {Nombre} {Apellido}, Bienvenido a Python.")
