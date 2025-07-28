61c6be37180c9476de53c3331cd163ea3b1965d1
ErickGP986 <Pichitron2004@gmail.com>
2025-07-28 12:35:44 -0600

    Create Variable10.py

---
+++ FUNDAMENTOS/VARIABLES/Variable10.py
@@ -0,0 +1,13 @@
+"""""
+Múltiples Asignaciones
+Asigna tres valores a tres variables en una sola línea: x, y, z = 10, 20, 30.
+Luego, usa += para sumar 5 a cada una.
+"""""""""
+x, y, z = 10, 20, 30  #creamos las tres variables y le asignamos los numeros
+x += 5 #Le sumamos 5 a cada una
+y += 5
+z += 5
+#Lo imprimimos
+print(x)
+print(y)
+print(z)
