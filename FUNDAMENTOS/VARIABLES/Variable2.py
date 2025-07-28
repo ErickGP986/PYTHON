733d997b349af396ab6ac448b2f39ea5401f5020
ErickGP986 <Pichitron2004@gmail.com>
2025-07-28 12:28:29 -0600

    Update Variable2.py

--- FUNDAMENTOS/VARIABLES/Variable2.py
+++ FUNDAMENTOS/VARIABLES/Variable2.py
@@ -1,1 +1,14 @@
-
+#Declaracion de Variables
+Num1 = int(input("Digite un primer numero: "))
+Num2 = int(input("Digite un segundo numero: "))
+#Hacemos las operaciones correspondidas
+suma = int(Num1 + Num2) #Convertimos a int Directamente
+resta = int(Num1 - Num2)
+multiplicacion = int(Num1 * Num2)
+division = int(Num1 / Num2)
+#Imprimimos los resultados
+print("Los numeros son: ", Num1, " ", Num2)
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multiplicacion)
+print("La division es: ", division)
