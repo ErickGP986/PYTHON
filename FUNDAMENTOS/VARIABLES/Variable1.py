20e72b99aac059e303689f0e17b40b1a821510d1
ErickGP986 <Pichitron2004@gmail.com>
2025-07-28 12:25:56 -0600

    Update Variable1.py

--- FUNDAMENTOS/VARIABLES/Variable1.py
+++ FUNDAMENTOS/VARIABLES/Variable1.py
@@ -1,1 +1,8 @@
-
+#Declaracion de Variables
+Nombre = input("Ingrese su nombre: ") 
+Edad = int(input("Ingrese su edad: ")) #Convertimos a entero directamente
+estudiando = input("Estas estudiando (si/no): ").lower == "si" #Convertimos a booleano
+#Lo imprimimos
+print(Nombre)
+print(Edad)
+print(estudiando)
