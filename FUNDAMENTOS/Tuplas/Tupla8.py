7b5b16cd393ea5b80626765ed59a422a8105cd2c
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:48:53 -0600

    Create Tupla8.py

---
+++ FUNDAMENTOS/Tuplas/Tupla8.py
@@ -0,0 +1,8 @@
+'''
+Tuplas anidadas:
+
+Dada la tupla matriz = ((1, 2), (3, 4)), accede al elemento 3 (fila 1, columna 0).
+'''
+matriz = ((1, 2), (3, 4)) #creamos la matriz
+
+print(f"El elemento es: {matriz[1][0]}") #acceedemos al elemento 3
