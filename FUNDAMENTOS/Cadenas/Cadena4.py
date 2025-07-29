b5aa57f9418d1bfd2cb49ef0fb7f94ccf7443f86
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:18:51 -0600

    Create Cadena4.py

---
+++ FUNDAMENTOS/Cadenas/Cadena4.py
@@ -0,0 +1,21 @@
+"""""
+Extraer subcadenas:
+Dada la cadena s = "Programación en Python", extrae:
+
+Los primeros 5 caracteres.
+
+Los últimos 6 caracteres.
+
+La palabra "Python" (usando índices).
+
+python
+s = "Programación en Python"
+print(s[:5])       # Salida: "Progr"
+print(s[-6:])      # Salida: "Python"
+print(s[16:])      # Salida: "Python"
+"""""""""
+s = "Programacion en Python" #Creamos la cadena
+
+print(s[:5]) #salida: "progr"
+print(s[-6:]) #salida: "Python"
+print(s[16:]) #salida: "Python"
