553964643b637be5fc61270209c24a5e49d5251e
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:16:00 -0600

    Create Cadena2.py

---
+++ FUNDAMENTOS/Cadenas/Cadena2.py
@@ -0,0 +1,15 @@
+"""""
+Concatenación:
+Une dos cadenas ingresadas por el usuario y muestra el resultado.
+
+python
+cadena1 = input("Primera parte: ")
+cadena2 = input("Segunda parte: ")
+print("Resultado:", cadena1 + cadena2)
+"""""""""
+cadena1 = input("Ingrese una cadena de texto: ") #Le pedimos la primera cadena de texto al usuario
+cadena2 = input("Ingerse una segunda cadena de texto: ") #Le pedimos la segunda cadena
+#Lo imprimimos
+print(f"La primera parte es: {cadena1}")
+print(f"La segunda cadena es: {cadena2}")
+print(f"Concatenando las dos cadenas es: {cadena1 + " " + cadena2}") #Concatenamos las dos cadenas
