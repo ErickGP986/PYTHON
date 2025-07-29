5f423a0af5efd74e113f65638eca04a478e06e69
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:37:27 -0600

    Create Lista6.py

---
+++ FUNDAMENTOS/Listas/Lista6.py
@@ -0,0 +1,11 @@
+'''
+Contar ocurrencias:
+
+Dada la lista ["a", "b", "a", "c", "a", "b"], cuenta cuántas veces aparece la letra "a" (usa el método .count()).
+'''
+lista = ["a", "b", "a", "c", "a", "b"] #Creamos la lista
+
+aparece_A = lista.count("a") #contamos las veces que aparece 'a'
+
+print(f"La lista es: {lista}") #imprimimos la lista
+print(f"La letra 'a' aparece: {aparece_A} veces") #imprimimos las veces que aparece 'a'
