07fbc3371d1ae24a57bafc134530f76081d16747
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:20:12 -0600

    Create Cadena6.py

---
+++ FUNDAMENTOS/Cadenas/Cadena6.py
@@ -0,0 +1,9 @@
+"""""
+Reemplazo de palabras:
+Dada la cadena "Amo JavaScript", reemplaza "JavaScript" por "Python" (usa .replace()).
+"""""""""
+
+cadena = "Amo JavaScript" #Creamos la cadena
+
+print(f"La cadena original es: {cadena}") #Imprimimos la cadena original
+print(f"Cadena remplazada: {cadena.replace("JavaScript", "Python")}") #Remplazamos el caracter y imprimimos
