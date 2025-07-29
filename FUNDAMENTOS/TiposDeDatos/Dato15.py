a9cf7d6f62d9fae6b6388629d82a123f83c6ef51
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 00:55:51 -0600

    Create Dato15.py

---
+++ FUNDAMENTOS/TiposDeDatos/Dato15.py
@@ -0,0 +1,11 @@
+"""""
+ Eliminar duplicados:
+- Dada la lista [3, 5, 3, 8, 5], conviértela en un set para eliminar duplicados.
+"""""""""
+lista = [3, 5, 3, 8, 5] #Creamos la lista 
+
+print(f"Lista original: {lista}") #La imprimimos
+
+lista_set = set(lista) #convertimos la lista a set
+
+print(f"Lista convertida en set: {lista_set}") #La imprimimos
