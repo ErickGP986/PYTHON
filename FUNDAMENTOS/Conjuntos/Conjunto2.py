ed95d70be7d18c4d6e1b4486cf0a4178ded1de81
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 02:24:45 -0600

    Create Conjunto2.py

---
+++ FUNDAMENTOS/Conjuntos/Conjunto2.py
@@ -0,0 +1,10 @@
+'''
+Eliminar elementos:
+
+Dado numeros = {1, 2, 3, 4, 5}, elimina el número 3 (usa .remove() o .discard()).
+'''
+numeros = {1, 2, 3, 4, 5} #creamos el conjunto numeros
+
+numeros.remove(3) #borramos el 3
+
+print(numeros) #imprimimos el conjunto
