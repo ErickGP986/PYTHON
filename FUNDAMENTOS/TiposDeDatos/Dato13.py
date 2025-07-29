0227c09690aa7ecf2d39e6b834a56a1b66f3d706
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 00:55:06 -0600

    Create Dato13.py

---
+++ FUNDAMENTOS/TiposDeDatos/Dato13.py
@@ -0,0 +1,12 @@
+"""""
+Diccionario de contacto:
+- Crea un diccionario contacto con las claves "nombre", "edad" y "email".
+- Luego, añade la clave "ciudad" con un valor.
+"""""""""
+contacto = {"nombre" : "Erick", "edad" : 20, "email" : "erick@gmail.com"} #Creamos el Diccionario
+
+print(f"Diccionario original {contacto}") #Lo imprimimos
+
+contacto["Ciudad"] = "Madrid" #Lo actualizamos
+#Lo imprimimos
+print(f"Diccionario actualizado: {contacto}")
