3354d195d6c077002d16c6d5e531ca0aeab5b947
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:35:40 -0600

    Create Lista3.py

---
+++ FUNDAMENTOS/Listas/Lista3.py
@@ -0,0 +1,13 @@
+'''
+Concatenar listas:
+
+Combina dos listas lista1 = [1, 2, 3] y lista2 = [4, 5, 6] en una sola lista llamada lista3.
+'''
+lista1 = [1, 2, 3] #Creamos la lista1
+lista2 = [4, 5, 6] #creamos la lista2
+
+lista3 = lista1 + lista2 #concatenamos las dos listas
+
+print(f"La primera lista: {lista1}") #imprimimos la primera lista
+print(f"La segunda lista: {lista2}") #imprimimos la segunda lista
+print(f"La concatenacion de las dos listas en una: {lista3}") #imprimimos la concatenacion de las listas
