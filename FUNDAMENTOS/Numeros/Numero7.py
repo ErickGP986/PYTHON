b779d69b0bae3b13ce19590043997f6f8319006a
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:05:19 -0600

    Create Numero7.py

---
+++ FUNDAMENTOS/Numeros/Numero7.py
@@ -0,0 +1,8 @@
+"""""
+Números aleatorios:
+Genera un número entero aleatorio entre 1 y 100 (usa random.randint(1, 100)).
+"""""""""
+import random #Importamos la libreria random
+Numero = random.randint(1,100) #Creamos la variable para que nos de un numero entre 1 y 100
+
+print(f"El numero aleatorio es: {Numero}") #Lo imprimimos
