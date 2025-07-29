42efd873f144a2d39158e0b1f4899fe31ff07502
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 02:44:25 -0600

    Create Conjunto13.py

---
+++ FUNDAMENTOS/Conjuntos/Conjunto13.py
@@ -0,0 +1,20 @@
+'''
+Verificar pertenencia
+
+Crea un set de caracteres: {'a', 'e', 'i', 'o', 'u'}
+
+Verifica si 'x' está en el set.
+
+Verifica si 'a' está en el set.
+'''
+caracteres = {'a', 'e', 'i', 'o', 'u'} #creamos el conjunto
+
+if 'x' in caracteres: #vemos si x esta en el conjunto
+    print(f"x si esta en caracteres {caracteres}") #impprimimimos si, si
+else: #si no
+    print(f"x no esta en caracteres: {caracteres}") #imprimimos si no esta
+
+if 'a' in caracteres: #vemos si a esta en el conjunto
+    print(f"a si esra en caracteres: {caracteres}") #imprimimos si esta
+else: #si no
+    print(f"a no esta en caracteres: {caracteres}") #imprimimos si no
