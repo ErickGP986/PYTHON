4a6628551b77cffbe6ec1ded2b9fc3fb8000ae3c
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 02:23:42 -0600

    Create Conjunto1.py

---
+++ FUNDAMENTOS/Conjuntos/Conjunto1.py
@@ -0,0 +1,12 @@
+'''
+Crear un set:
+
+Define un set vocales = {"a", "e", "i", "o", "u"} e imprímelo.
+
+Intenta añadir una vocal repetida (ej: "a") y verifica que no se duplica.
+'''
+vocales = {"a", "e", "i", "o", "u"} #creamos el set de vocales
+
+vocales.add("a") #le añadimos otra vez a
+
+print(vocales) #imprimimos para ver que no se duplique
