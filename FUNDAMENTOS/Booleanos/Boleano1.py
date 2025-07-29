84a004081b0c423d45fbdaf32ae5f1c2672ecab9
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:24:42 -0600

    Create Boleano1.py

---
+++ FUNDAMENTOS/Booleanos/Boleano1.py
@@ -0,0 +1,17 @@
+'''
+Mayor o menor:
+Pide dos números al usuario y muestra:
+
+True si el primero es mayor que el segundo.
+
+False si no lo es.
+
+python
+num1 = int(input("Número 1: "))
+num2 = int(input("Número 2: "))
+print(num1 > num2)  # ¿Es mayor?
+'''
+num1 = int(input("Ingrese el primer numero: ")) #Le pedimo el primer numero al usuario
+num2 = int(input("Ingrese el segundo numero: ")) #Le pedimos el segundo numero al usuario
+
+print(num1 > num2) #Preguntamos si es mayor el primero que el segundo
