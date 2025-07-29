7f403ae603fd70351fe5f76d3657ea6060715385
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:18:28 -0600

    Create Cadena3.py

---
+++ FUNDAMENTOS/Cadenas/Cadena3.py
@@ -0,0 +1,13 @@
+"""""
+Repetición:
+Pide una palabra y un número, luego repite la palabra *n* veces.
+
+palabra = input("Palabra: ")
+n = int(input("Repeticiones: "))
+print(palabra * n)
+"""""""""
+
+palabra = input("Ingrese una palabra: ") #Le pedimos al usuario la palabra
+n = int(input("Ingrese cuantas veces quiere que se repita: ")) #Le decimos cuantas veces quiere que se repita
+
+print(palabra * n)#Lo imprimimos
