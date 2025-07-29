b5f0832421c01d54633fd1ced8b7fcbd7c65083e
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:42:21 -0600

    Create Tupla3.py

---
+++ FUNDAMENTOS/Tuplas/Tupla3.py
@@ -0,0 +1,13 @@
+'''
+Concatenar tuplas:
+
+Combina dos tuplas tupla1 = (1, 2) y tupla2 = (3, 4) en una nueva tupla tupla3.
+'''
+tupla1 = (1, 2) #creamos la primera tupla
+tupla2 = (3, 4)#creamos la segunda tupla
+
+tupla3 = tupla1 + tupla2 #concatenamos las tuplas en una
+
+print(f"La primera tupla: {tupla1}") #imprimimmos la primera tabla
+print(f"La segunda tupla es: {tupla2}") #imprimimmos la segunda tabla
+print(f"Las tuplas concatenadas: {tupla3}") #imprimimos las tuplas concatenadas
