b1eaf8ae20e56ed6b28351ff7dcc555a77cc524d
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:19:43 -0600

    Create Cadena5.py

---
+++ FUNDAMENTOS/Cadenas/Cadena5.py
@@ -0,0 +1,16 @@
+"""""
+Mayúsculas y minúsculas:
+Convierte una cadena ingresada por el usuario a:
+
+Todo mayúsculas (.upper()).
+
+Todo minúsculas (.lower()).
+
+Formato título (.title()).
+"""""""""
+cadena = input("Ingrese una cadena de texto: ") #Le pedimos al usuario que cree la cadena de texto
+
+print(f"cadena original: {cadena}") #Imprimimos la original
+print(f"La cadena en mayusculas: {cadena.upper()}") #Imprimimos en mayusculas
+print(f"La cadena en minusculas: {cadena.lower()}") #Imrpimimos en minusculas
+print(f"La cadena en titulo: {cadena.title()}") #Imprimimos en titulo
