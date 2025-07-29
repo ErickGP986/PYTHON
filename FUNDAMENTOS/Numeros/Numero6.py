96570952e2db2c6b83ce5d73f4b776ef88a58e4b
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:04:56 -0600

    Create Numero6.py

---
+++ FUNDAMENTOS/Numeros/Numero6.py
@@ -0,0 +1,8 @@
+"""""
+Número absoluto:
+Pide un número (positivo o negativo) y muestra su valor absoluto (usa abs())
+"""""""""
+#Creamos la variable
+numero = int(input("Escriba un numero positivo o negativo: "))
+#lo imprimimos con el valor absoluto
+print(f"El valor absoluto del numero es: {abs(numero)}")
