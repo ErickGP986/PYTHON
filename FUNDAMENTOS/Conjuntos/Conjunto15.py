e617deaa6c1c1d30d4cb5905204cedfdc9309e24
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 02:45:07 -0600

    Create Conjunto15.py

---
+++ FUNDAMENTOS/Conjuntos/Conjunto15.py
@@ -0,0 +1,13 @@
+'''
+Intersección
+
+Usando los sets anteriores, calcula la intersección
+'''
+s1 = {1, 2, 3} #creamos el primer set
+s2 = {3, 4, 5} #creamos el segundo set
+
+dos_set = s1.intersection(s2) #creamos la interseccion del primer y segundo conjunto
+
+print(f"El primer set es: {s1}") #imprimimos el primer set
+print(f"El segundo set es: {s2}") #imprimimmos le segundo set
+print(f"La interseccion de los set es: {dos_set}") #imprimimmos la interseccion
