340f0d0c4224e87256d440b7f6d9fc7fec026dad
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:03:45 -0600

    Create Numero4.py

---
+++ FUNDAMENTOS/Numeros/Numero4.py
@@ -0,0 +1,7 @@
+"""""
+Redondear decimales:
+Pide un número float y redondéalo a 2 decimales (usa round(numero, 2)).
+"""""""""
+numero = float(input("Digite un numero decimal: ")) #Creamos la variable y lo pedimos al usuario
+
+print(f"El numero es: {round(numero, 2)}") #Lo imprimimos y lo redondeamos a 2 decimales
