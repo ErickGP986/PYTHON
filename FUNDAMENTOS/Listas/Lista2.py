5a3ba5e7fa95e992e714a2cadfa1e95d23a143d5
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:34:27 -0600

    Create Lista2.py

---
+++ FUNDAMENTOS/Listas/Lista2.py
@@ -0,0 +1,18 @@
+'''
+Acceder a elementos:
+
+Dada la lista numeros = [10, 20, 30, 40, 50], imprime:
+
+El primer elemento.
+
+El último elemento.
+
+Los elementos del segundo al cuarto (usando slicing).
+'''
+numeros = [10, 20, 30, 40, 50] #Creamos la lista
+
+print(f"El primer elemento: {numeros[0]}") #Imprimimos el primer elemento
+
+print(f"El ultimo elemento: {numeros[-1]}") #Imprimimos el ultimo elemento
+
+print(f"Elementos del segundo al cuarto {numeros[1:4]}") #Imprimimos los elementos del segundo al cuarto
