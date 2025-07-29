0c2d2d8cb3a544dc818fb50f3096add04b129ba1
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:38:25 -0600

    Create Lista8.py

---
+++ FUNDAMENTOS/Listas/Lista8.py
@@ -0,0 +1,9 @@
+'''
+Invertir una lista:
+
+Invierte el orden de la lista ["Python", "Java", "C++"] sin usar reverse() (usa slicing).
+'''
+lista = ["Python", "Java", "C++"]  #creamos la lista
+
+print(f"la lista original es: {lista}") #imprimimos la lista original
+print(f"la lista en reversa es: {lista[::-1]}") #imprimimos la lista al reves con slicing
