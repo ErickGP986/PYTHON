d808cc5669e1891ac156547e83e2ead93465b729
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:43:24 -0600

    Create Tupla5.py

---
+++ FUNDAMENTOS/Tuplas/Tupla5.py
@@ -0,0 +1,11 @@
+'''
+Buscar índice:
+
+En la tupla frutas = ("manzana", "banana", "naranja"), encuentra el índice de "banana" (usa .index()).
+'''
+frutas = ("manzana", "banana", "naranja") #creamos la tupla
+
+buscar_banana = frutas.index("banana") #buscamos si existe banana
+
+print(f"las frutas son: {frutas}") #imprimimos la tupla frutas
+print(f"Existe banana en las frutas: {buscar_banana}") #imrimimos el indice de banana
