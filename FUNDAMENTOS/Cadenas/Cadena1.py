0a01652f664805be3249ab6d88e6c8610a92d168
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:15:37 -0600

    Create Cadena1.py

---
+++ FUNDAMENTOS/Cadenas/Cadena1.py
@@ -0,0 +1,9 @@
+"""""
+Longitud de una cadena:
+Pide al usuario que ingrese una palabra y muestra su longitud (usa len()).
+"""""""""
+
+texto = input("Digite una cadena de texto: ") #Le pedimos al usuario una cadena de texto
+
+print(f"La cadena es: {texto}") #Imprimimos la cadena
+print(f"La longitud de la cadena es: {len(texto)}") #Mostramos la longitud de la cadena
