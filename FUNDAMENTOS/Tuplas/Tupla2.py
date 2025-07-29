4c4427b76fc83606de59f1f64b9df8fc5f8ff512
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:42:00 -0600

    Create Tupla2.py

---
+++ FUNDAMENTOS/Tuplas/Tupla2.py
@@ -0,0 +1,13 @@
+'''
+Acceder a elementos:
+
+Dada la tupla punto = (3, 5), imprime:
+
+El primer elemento (coordenada x).
+
+El segundo elemento (coordenada y).
+'''
+punto = (3, 5) #creamos la tupla
+
+print(f"Coordenada x: {punto[0]}") #imprimimmos la coordenada x
+print(f"Coordenada y: {punto[1]}") #imprimimos la coordenada y
