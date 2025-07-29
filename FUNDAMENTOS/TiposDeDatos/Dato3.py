5f317aa473c93cd209717920ba9cc94cd9ae3d76
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 00:44:49 -0600

    Create Dato3.py

---
+++ FUNDAMENTOS/TiposDeDatos/Dato3.py
@@ -0,0 +1,10 @@
+"""""
+Par o impar:
+Pide un número entero y determina si es par o impar (usa el operador %).
+"""""""""
+Numero = int(input("Digite un numero entero: "))
+
+if(Numero % 2 == 0):
+    print("El numero es par")
+else:
+    print("El numero es impar.")
