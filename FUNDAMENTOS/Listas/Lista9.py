f76a3ef6a4eedfb71de933db68b341025cd13faa
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:38:48 -0600

    Create Lista9.py

---
+++ FUNDAMENTOS/Listas/Lista9.py
@@ -0,0 +1,12 @@
+'''
+Lista de listas (matriz):
+
+Crea una matriz 2x2 (lista de listas) y accede al elemento en la segunda fila y primera columna:
+
+python
+matriz = [[1, 2], [3, 4]]
+print(matriz[1][0])
+'''
+matriz = [[1, 2], [3, 4]] #creamos la matriz
+
+print(matriz[1][0]) #imprimimos el elemento que nos dice
