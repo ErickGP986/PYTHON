b04cb52a8bcfa328dbebf33fdc7911128dc02360
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 02:42:04 -0600

    Create Conjunto10.py

---
+++ FUNDAMENTOS/Conjuntos/Conjunto10.py
@@ -0,0 +1,13 @@
+'''
+Crear un set
+
+Crea un set con los números del 1 al 5.
+
+Imprímelo y verifica su longitud.
+'''
+numeros = {1, 2 ,3 , 4, 5} #creamos el conjunto
+
+longitud = len(numeros) #vemos cual es su longitud
+
+print(numeros) #imprimimos los numeros
+print(longitud) #imprimimos la longitud
