b8ba2b4dd9351e913de1676a5fd76e378554b361
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:43:06 -0600

    Create Tupla4.py

---
+++ FUNDAMENTOS/Tuplas/Tupla4.py
@@ -0,0 +1,11 @@
+'''
+Contar ocurrencias:
+
+Dada la tupla datos = (10, 20, 10, 30, 10), cuenta cuántas veces aparece el número 10 (usa .count()).
+'''
+datos = (10, 20, 10, 30, 10) #creamos la tupla
+
+aparece_10 = datos.count(10) #contamos las veces que aparece 10
+
+print(f"los datos son: {datos}") #imprimimos la tupla
+print(f"las veces que aparece 10 es: {aparece_10}") #imprimimos el contadoe de 10
