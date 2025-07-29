3996a3d2df630f0ad910a452e748136402bf1806
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 00:57:40 -0600

    Create Dato16.py

---
+++ FUNDAMENTOS/TiposDeDatos/Dato16.py
@@ -0,0 +1,12 @@
+"""""
+Intersección de sets:
+Dados set_a = {1, 2, 3} y set_b = {3, 4, 5}, imprime los elementos comunes.
+Intersección de sets:
+Dados set_a = {1, 2, 3} y set_b = {3, 4, 5}, imprime los elementos comunes.
+"""""""""
+set_a = {1, 2, 3} #Creamos los set
+set_b = {3, 4, 5}
+#Creamos la interseccion
+inserccion_set = set(set_a) & set(set_b)
+#Lo imprimimos
+print(inserccion_set)
