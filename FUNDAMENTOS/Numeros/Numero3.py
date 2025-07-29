6d5b2d29c0527f64573111010b0490e60dc43d73
ErickGP986 <Pichitron2004@gmail.com>
2025-07-29 01:03:22 -0600

    Create Numero3.py

---
+++ FUNDAMENTOS/Numeros/Numero3.py
@@ -0,0 +1,10 @@
+"""""
+Par o impar:
+Pide un número entero y determina si es par o impar (usa %).
+"""""""""
+Num = int(input("Digite un numero entero: ")) #Creamos la variable y lo pedimos al usuario
+#Hacemos la condicion 
+if(Num % 2 == 0):
+ print(f"El numero {Num} es par.") #Lo imprimimos
+else:
+ print(f"El numero {Num} no es par.")    
